c87f4249d3a31d11125791dc93c694bb2eb3954a
Reynaldo C. Depagon <55631616+depagon100@users.noreply.github.com>
2023-02-21 18:39:55 +0800

    Updated action button dashboards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -59,12 +59,14 @@
                                 <td>33</td>
                                 <td>2008/11/28</td>
                                 <td>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-info btn-sm">Edit</button>
-                                        <button type="button" class="btn btn-warning btn-sm">Update</button>
-                                        <button type="button" class="btn btn-danger btn-sm"
-                                                style="color:black">Delete</button>
-                                    </div>
+                                    <button class="btn btn-info"><i class="bi bi-pencil-square"></i><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd"
+                                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        </svg></button>
                                 </td>
                             </tr>
                             <tr role="row" class="even">
@@ -74,12 +76,14 @@
                                 <td>47</td>
                                 <td>2009/10/09</td>
                                 <td>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-info btn-sm">Edit</button>
-                                        <button type="button" class="btn btn-warning btn-sm">Update</button>
-                                        <button type="button" class="btn btn-danger btn-sm"
-                                                style="color:black">Delete</button>
-                                    </div>
+                                    <button class="btn btn-info"><i class="bi bi-pencil-square"></i><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd"
+                                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        </svg></button>
                                 </td>
                             </tr>
 
@@ -106,34 +110,41 @@
                 </div>--}}
 
 
-                    <div id="example_wrapper" class="dataTables_wrapper no-footer">
-
-                        <table id="example1" class="stripe hover dataTable no-footer dtr-inline" style="width: 100%;" role="grid"
-                               aria-describedby="example_info">
+                    <div id="example_wrapper" class="mb-2 dataTables_wrapper no-footer mr-2">
+                        <table id="example1" class="stripe hover dataTable no-footer dtr-inline" style="width: 100%;"
+                               role="grid" aria-describedby="example_info">
                             <thead>
-                            <tr role="row" style="width: 100%;">
+                            <tr role="row">
                                 <th data-priority="1" class="sorting_asc" tabindex="0" aria-controls="example"
-                                    aria-sort="ascending"
-                                    aria-label="Emb Id: activate to sort column descending">EMB Id</th>
-                                <th data-priority="2" class="sorting" tabindex="0" aria-controls="example" rowspan="1"
-                                    aria-label="Facility: activate to sort column ascending">Establishment
-                                </th>
-                                <th data-priority="3" class="sorting" tabindex="0" aria-controls="example" rowspan="1"
-                                    aria-label="Date Created: activate to sort column ascending">Street</th>
-                                <th data-priority="4" class="sorting" tabindex="0" aria-controls="example" rowspan="1"
-                                    aria-label="Date Submitted: activate to sort column ascending">Baranggay
-                                </th>
-                                <th data-priority="5" class="sorting" tabindex="0" aria-controls="example" rowspan="1"
-                                    aria-label="Date Created: activate to sort column ascending">
-                                    City/Muncipality</th>
-                                <th data-priority="6" class="sorting" tabindex="0" aria-controls="example" rowspan="1"
-                                    aria-label="Date Submitted: activate to sort column ascending">Province
-                                </th>
-                                <th data-priority="7" class="sorting" tabindex="0" aria-controls="example" rowspan="1"
-                                    aria-label="Status: activate to sort column ascending">Date Approved
-                                </th>
-                                <th data-priority="7" class="sorting" tabindex="0" aria-controls="example" rowspan="1"
-                                    aria-label="Status: activate to sort column ascending">Status</th>
+                                    rowspan="1" colspan="1" style="width: 142px;" aria-sort="ascending"
+                                    aria-label="Reference No.: activate to sort column descending">EMB Id.</th>
+                                <th data-priority="2" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 224px;"
+                                    aria-label="Facility: activate to sort column ascending">Establishment</th>
+                                <th data-priority="3" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 100px;"
+                                    aria-label="Date Created: activate to sort column ascending">Name</th>
+                                <th data-priority="4" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 100px;"
+                                    aria-label="Date Submitted: activate to sort column ascending">Street</th>
+                                <th data-priority="5" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 92px;"
+                                    aria-label="Status: activate to sort column ascending">Baranggay</th>
+                                <th data-priority="6" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 77px;"
+                                    aria-label="Action: activate to sort column ascending">City/Muncipality</th>
+                                <th data-priority="7" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 77px;"
+                                    aria-label="Action: activate to sort column ascending">Province</th>
+                                <th data-priority="8" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 77px;"
+                                    aria-label="Action: activate to sort column ascending">Date Approved</th>
+                                <th data-priority="9" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 77px;"
+                                    aria-label="Action: activate to sort column ascending">Status</th>
+                                <th data-priority="10" class="sorting" tabindex="0" aria-controls="example"
+                                    rowspan="1" colspan="1" style="width: 77px;"
+                                    aria-label="Action: activate to sort column ascending">Action</th>
                             </tr>
 
                             </thead>
@@ -142,46 +153,98 @@
 
 
                             <tr role="row" class="odd">
-                                <td>Airi Satou</td>
+                                <td tabindex="0" class="sorting_1">Airi Satou</td>
                                 <td>Accountant</td>
                                 <td>Tokyo</td>
                                 <td>33</td>
                                 <td>2008/11/28</td>
                                 <td>$162,700</td>
-                                <td>hdfsdfsss</td>
+                                <td>dasd</td>
+                                <td>dasdas</td>
+                                <td>Online</td>
                                 <td>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-info btn-sm">Edit</button>
-                                        <button type="button" class="btn btn-warning btn-sm">Update</button>
-                                        <button type="button" class="btn btn-danger btn-sm"
-                                                style="color:black">Delete</button>
-                                    </div>
+                                    <button class="btn btn-info"><i class="bi bi-pencil-square"></i><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd"
+                                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        </svg></button>
                                 </td>
                             </tr>
                             <tr role="row" class="even">
                                 <td class="sorting_1" tabindex="0">Angelica Ramos</td>
-                                <td>CEO</td>
+                                <td>Chief Executive Officer (CEO)</td>
                                 <td>London</td>
                                 <td>47</td>
                                 <td>2009/10/09</td>
                                 <td>$1,200,000</td>
-                                <td>hdfsdfsss</td>
+                                <td>dasda</td>
+                                <td>dasdas</td>
+                                <td>Offline</td>
                                 <td>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-info btn-sm">Edit</button>
-                                        <button type="button" class="btn btn-warning btn-sm">Update</button>
-                                        <button type="button" class="btn btn-danger btn-sm"
-                                                style="color:black">Delete</button>
-                                    </div>
+                                    <button class="btn btn-info"><i class="bi bi-pencil-square"></i><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd"
+                                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        </svg></button>
+                                </td>
+
+                            </tr>
+                            <tr role="row" class="odd">
+                                <td tabindex="0" class="sorting_1">Ashton Cox</td>
+                                <td>Junior Technical Author</td>
+                                <td>San Francisco</td>
+                                <td>66</td>
+                                <td>2009/01/12</td>
+                                <td>$86,000</td>
+                                <td>dasda</td>
+                                <td>dasd</td>
+                                <td>Online</td>
+                                <td>
+                                    <button class="btn btn-info"><i class="bi bi-pencil-square"></i><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd"
+                                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        </svg></button>
                                 </td>
                             </tr>
+                            <tr role="row" class="even">
+                                <td class="sorting_1" tabindex="0">Bradley Greer</td>
+                                <td>Software Engineer</td>
+                                <td>London</td>
+                                <td>41</td>
+                                <td>2012/10/13</td>
+                                <td>$132,000</td>
+                                <td>asda</td>
+                                <td>dasdas</td>
+                                <td>Offline</td>
+                                <td>
+                                    <button class="btn btn-info"><i class="bi bi-pencil-square"></i><svg
+                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path
+                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd"
+                                                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        </svg></button>
+                                </td>
 
+
+                                </td>
+                            </tr>
                             </tbody>
 
                         </table>
 
                     </div>
-
 
                 </div>
 
